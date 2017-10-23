7913375113ca996aa78c9e2cceb11730214d08bd
Jeremy Lewi <jeremy@lewi.us>
2017-10-23 16:10:40 -0700

    Fix location of the post submit results. (#74)
    * GCS location needs to include github organization and name.

--- test-infra/runner.py
+++ test-infra/runner.py
@@ -255,7 +255,7 @@ def get_gcs_output():
     return output
   else:
     # It is a periodic or postsubmit job
-    output = ("gs://kubernetes-jenkins/logs/"
+    output = ("gs://kubernetes-jenkins/logs/{owner}_{repo}/"
               "{job}/{build}").format(
                   owner=GO_REPO_OWNER, repo=GO_REPO_NAME,
                   pull_number=pull_number,
