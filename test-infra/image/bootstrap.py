aa4719e2952b75b826d1d4d13c157ad88d93fb54
Jeremy Lewi <jeremy@lewi.us>
2017-10-25 14:55:13 -0700

    Got a minimal example submitting a job and getting the status.

--- test-infra/image/bootstrap.py
+++ test-infra/image/bootstrap.py
@@ -28,8 +28,8 @@ import shutil
 
 # Default name for the repo organization and name.
 # This should match the values used in Go imports.
-GO_REPO_OWNER = "jlewi"
-GO_REPO_NAME = "mlkube.io"
+GO_REPO_OWNER = "tensorflow"
+GO_REPO_NAME = "k8s"
 
 
 def run(command, cwd=None):
