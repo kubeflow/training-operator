65df418b3bf03cc7998f60aa2b4441ecf49090dc
Jeremy Lewi <jeremy@lewi.us>
2017-11-02 19:39:48 -0700

    Move a lot of the code into util.py.
    * There's a problem with 1.8 clusters because rbac isn't setup correctly.

--- py/util.py
+++ py/util.py
@@ -6,6 +6,13 @@ import logging
 import os
 import shutil
 import subprocess
+import time
+import urllib
+import yaml
+
+from googleapiclient import errors
+from kubernetes import client as k8s_client
+from kubernetes.client import rest
 
 # Default name for the repo organization and name.
 # This should match the values used in Go imports.
@@ -93,6 +100,36 @@ def to_gcs_uri(bucket, path):
   return "gs://" + os.path.join(bucket, path)
 
 
+def create_cluster(gke, project, zone, cluster_request):
+  """Create the cluster.
+
+  Args:
+    gke: Client for GKE.
+    project: The project to create the cluster in
+    zone: The zone to create the cluster in.
+    cluster_rquest: The request for the cluster.
+  """
+  request = gke.projects().zones().clusters().create(body=cluster_request,
+                                                     projectId=project,
+                                                     zone=zone)
+
+  try:
+    logging.info("Creating cluster; project=%s, zone=%s, name=%s", project,
+                 zone, cluster_request["cluster"]["name"])
+    response = request.execute()
+    logging.info("Response %s", response)
+    create_op = wait_for_operation(gke, project, zone, response["name"])
+    logging.info("Cluster creation done.\n %s", create_op)
+
+  except errors.HttpError as e:
+    logging.error("Exception occured creating cluster: %s, status: %s",
+                  e, e.resp["status"])
+    # Status appears to be a string.
+    if e.resp["status"] == '409':
+      pass
+    else:
+      raise
+
 def delete_cluster(gke, name, project, zone):
   """Delete the cluster.
 
@@ -159,3 +196,140 @@ def wait_for_operation(client,
       raise TimeoutError("Timed out waiting for op: {0} to complete.".format(
         op_id))
     time.sleep(polling_interval.total_seconds())
+
+def configure_kubectl(project, zone, cluster_name):
+  logging.info("Configuring kubectl")
+  run(["gcloud", "--project=" + project, "container",
+       "clusters", "--zone=" + zone, "get-credentials", cluster_name])
+
+def wait_for_tiller_to_be_ready(api_client):
+  # Wait for tiller to be ready
+  end_time = datetime.datetime.now() + datetime.timedelta(minutes=2)
+
+  ext_client = k8s_client.ExtensionsV1beta1Api(api_client)
+
+  tiller_ready = False
+  while datetime.datetime.now() < end_time:
+    deploy = ext_client.read_namespaced_deployment("tiller-deploy", "kube-system")
+    if deploy.status.ready_replicas >= 1:
+      logging.info("tiller is ready")
+      tiller_ready = True
+      return
+    logging.info("Waiting for tiller")
+    time.sleep(10)
+
+  raise ValueError("Timeout waiting for tiller")
+
+def install_gpu_drivers(api_client):
+  """Install GPU drivers on the cluster.
+
+  Note: GPU support in K8s is very much Alpha and this code will
+  likely change quite frequently.
+
+  Return:
+     ds: Daemonset for the GPU installer
+  """
+  logging.info("Install GPU Drivers.")
+  # Fetch the daemonset to install the drivers.
+  link = "https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/k8s-1.8/device-plugin-daemonset.yaml"  # pylint: disable=line-too-long
+  f = urllib.urlopen(link)
+  daemonset_spec = yaml.load(f)
+  ext_client = k8s_client.ExtensionsV1beta1Api(api_client)
+  try:
+    ds = ext_client.create_namespaced_daemon_set(daemonset_spec["metadata"]["namespace"], daemonset_spec)
+  except rest.ApiException as e:
+    # Status appears to be a string.
+    if e.status == 409:
+      logging.info("GPU driver daemon set has already been installed")
+    else:
+      raise
+
+def wait_for_gpu_driver_install(api_client,
+                                timeout=datetime.timedelta(minutes=10)):
+  """Wait until some nodes are available with GPUs."""
+
+  end_time = datetime.datetime.now() + timeout
+  api = k8s_client.CoreV1Api(api_client)
+  while datetime.datetime.now() <= end_time:
+    nodes = api.list_node()
+    for n in nodes.items:
+      if n.status.capacity.get("nvidia.com/gpu", 0) > 0:
+        logging.info("GPUs are available.")
+        return
+    logging.info("Waiting for GPUs to be ready.")
+    time.sleep(15)
+  logging.error("Timeout waiting for GPUs to be ready.")
+  raise TimeoutEr
+
+def cluster_has_gpu_nodes(api_client):
+  """Return true if the cluster has nodes with GPUs."""
+  api = k8s_client.CoreV1Api(api_client)
+  nodes = api.list_node()
+
+  for n in nodes.items:
+    if "cloud.google.com/gke-accelerator" in n.metadata.labels:
+      return True
+  return False
+
+def create_tiller_service_accounts(api_client):
+  logging.info("Creating service account for tiller.")
+  api = k8s_client.CoreV1Api(api_client)
+  body = yaml.load("""apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: tiller
+  namespace: kube-system""")
+  try:
+    api.create_namespaced_service_account("kube-system", body)
+  except rest.ApiException as e:
+    if e.status == 409:
+      logging.info("Service account tiller already exists.")
+    else:
+      raise
+  body = yaml.load("""apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: tiller
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+  - kind: ServiceAccount
+    name: tiller
+    namespace: kube-system
+""")
+  rbac_api = k8s_client.RbacAuthorizationV1beta1Api(api_client)
+  try:
+    rbac_api.create_cluster_role_binding(body)
+  except rest.ApiException as e:
+    if e.status == 409:
+      logging.info("Role binding for service account tiller already exists.")
+    else:
+      raise
+
+def setup_cluster(api_client):
+  """Setup a cluster.
+
+  This function assumes kubectl has already been configured to talk to your
+  cluster.
+
+  Args:
+    use_gpus
+  """
+  create_tiller_service_accounts(api_client)
+  run(["helm", "init", "--service-account=tiller"])
+  use_gpus = cluster_has_gpu_nodes(api_client)
+  if use_gpus:
+    logging.info("GPUs detected in cluster.")
+  else:
+    logging.info("No GPUs detected in cluster.")
+
+  if use_gpus:
+    install_gpu_drivers(api_client)
+  wait_for_tiller_to_be_ready(api_client)
+  if use_gpus:
+    wait_for_gpu_driver_install(api_client)
+
+class TimeoutError(Exception):
+  """An error indicating an operation timed out."""
