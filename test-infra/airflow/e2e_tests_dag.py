6b0bde135df5a1f1eb9884d9028d63676cc0d38d
Jeremy Lewi <jeremy@lewi.us>
2017-11-06 21:57:44 -0800

    Start trying to create a deployment for airflow.

---
+++ test-infra/airflow/e2e_tests_dag.py
@@ -0,0 +1,58 @@
+"""
+Code that goes along with the Airflow located at:
+http://airflow.readthedocs.org/en/latest/tutorial.html
+"""
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2015, 6, 1),
+    'email': ['airflow@airflow.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG(
+    # Set schedule_interval to None
+    'tf_k8s_tests', default_args=default_args, schedule_interval=None)
+
+# t1, t2 and t3 are examples of tasks created by instantiating operators
+t1 = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=dag)
+
+# t2 = BashOperator(
+#     task_id='sleep',
+#     bash_command='sleep 5',
+#     retries=3,
+#     dag=dag)
+
+# templated_command = """
+#     {% for i in range(5) %}
+#         echo "{{ ds }}"
+#         echo "{{ macros.ds_add(ds, 7)}}"
+#         echo "{{ params.my_param }}"
+#     {% endfor %}
+# """
+#
+for i in range(5):
+    t3 = BashOperator(
+        task_id='task_{0}'.format(i),
+        bash_command='echo task {0}'.format(i),
+        params={'my_param': 'Parameter I passed in'},
+        dag=dag)
+
+    t3.set_upstream(t1)
+#t2.set_upstream(t1)
+
