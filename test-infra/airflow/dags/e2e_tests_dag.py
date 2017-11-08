9cd338f73a4b1cbe37de2ee7a92d5d27363ebbb0
Jeremy Lewi <jeremy@lewi.us>
2017-11-07 18:22:42 -0800

    Pipeline executes successfully on K8s.

--- test-infra/airflow/dags/e2e_tests_dag.py
+++ test-infra/airflow/dags/e2e_tests_dag.py
@@ -47,19 +47,19 @@ def delete_cluster():
 
 build_op = PythonOperator(
     task_id='build_images',
-    provide_context=True,
+    provide_context=False,
     python_callable=build_images,
     dag=dag)
 
 create_cluster_op = PythonOperator(
     task_id='create_cluster',
-    provide_context=True,
+    provide_context=False,
     python_callable=create_cluster,
     dag=dag)
 
 setup_cluster_op = PythonOperator(
     task_id='setup_cluster',
-    provide_context=True,
+    provide_context=False,
     python_callable=setup_cluster,
     dag=dag)
 
@@ -68,7 +68,7 @@ setup_cluster_op.set_upstream(create_cluster_op)
 
 deploy_crd_op = PythonOperator(
     task_id='deploy_crd',
-    provide_context=True,
+    provide_context=False,
     python_callable=deploy_crd,
     dag=dag)
 
@@ -76,7 +76,7 @@ deploy_crd_op.set_upstream([setup_cluster_op, build_op])
 
 run_tests_op = PythonOperator(
     task_id='run_tests',
-    provide_context=True,
+    provide_context=False,
     python_callable=run_tests,
     dag=dag)
 
@@ -84,7 +84,7 @@ run_tests_op.set_upstream(deploy_crd_op)
 
 delete_cluster_op = PythonOperator(
     task_id='delete_cluster',
-    provide_context=True,
+    provide_context=False,
     python_callable=run_tests,
     dag=dag)
 
