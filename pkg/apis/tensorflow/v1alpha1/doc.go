1edafed1f81d2f4dcc17a073dc6dfb9e59192ede
wackxu <xushiwei5@huawei.com>
2017-12-19 11:25:20 +0800

    add code-generater shell script

--- pkg/apis/tensorflow/v1alpha1/doc.go
+++ pkg/apis/tensorflow/v1alpha1/doc.go
@@ -15,6 +15,7 @@ limitations under the License.
 */
 
 // +k8s:deepcopy-gen=package,register
+// +k8s:defaulter-gen=TypeMeta
 
 // Package v1alpha1 is the v1alpha1 version of the API.
 // +groupName=tensorflow.org
