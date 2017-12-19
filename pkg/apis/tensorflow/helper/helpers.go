b01d6f4c39dcc4f8e1d1a7f2353c2dc7d6f7bfdb
wackxu <xushiwei5@huawei.com>
2017-12-19 11:00:58 +0800

    refactor the spec to the standard k8s API group

---
+++ pkg/apis/tensorflow/helper/helpers.go
@@ -0,0 +1,95 @@
+package helper
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	tfv1 "github.com/tensorflow/k8s/pkg/apis/tensorflow/v1alpha1"
+	"fmt"
+	"k8s.io/api/core/v1"
+	"github.com/tensorflow/k8s/pkg/util"
+)
+
+// AsOwner make OwnerReference according to the parameter
+func AsOwner(tfJob *tfv1.TfJob) metav1.OwnerReference {
+	trueVar := true
+	// Both api.OwnerReference and metatypes.OwnerReference are combined into that.
+	return metav1.OwnerReference{
+		APIVersion:         tfJob.APIVersion,
+		Kind:               tfJob.Kind,
+		Name:               tfJob.ObjectMeta.Name,
+		UID:                tfJob.ObjectMeta.UID,
+		Controller:         &trueVar,
+		BlockOwnerDeletion: &trueVar,
+	}
+}
+
+// ConfigureAcceleratorsForTfJobSpec adds any accelerator specific configuration to the pods.
+func ConfigureAcceleratorsForTfJobSpec(c *tfv1.TfJobSpec, accelerators map[string]tfv1.AcceleratorConfig) error {
+	for _, r := range c.ReplicaSpecs {
+		if r.Template == nil {
+			return fmt.Errorf("Replica is missing Template; %v", util.Pformat(r))
+		}
+		for i, c := range r.Template.Spec.Containers {
+			if c.Name == string(tfv1.TENSORFLOW) {
+				// Identify the accelerators attached to this container.
+				a := map[string]tfv1.AcceleratorConfig{}
+
+				lists := []v1.ResourceList{c.Resources.Limits, c.Resources.Requests}
+				for _, resources := range lists {
+					for name, _ := range resources {
+
+						if _, ok := accelerators[string(name)]; !ok {
+							continue
+						}
+
+						// Add the expected mounts to the pods.
+						a[string(name)] = accelerators[string(name)]
+					}
+				}
+
+				// Add accelerator information to the pod.
+				for _, config := range a {
+					for _, v := range config.Volumes {
+						r.Template.Spec.Volumes = append(r.Template.Spec.Volumes,
+							v1.Volume{
+								Name: v.Name,
+								VolumeSource: v1.VolumeSource{
+									HostPath: &v1.HostPathVolumeSource{
+										Path: v.HostPath,
+									},
+								},
+							})
+						c.VolumeMounts = append(c.VolumeMounts, v1.VolumeMount{
+							Name:      v.Name,
+							MountPath: v.MountPath,
+						})
+					}
+
+					for _, envVar := range config.EnvVars {
+						c.Env = append(c.Env, v1.EnvVar{
+							Name:  envVar.Name,
+							Value: envVar.Value,
+						})
+					}
+				}
+				r.Template.Spec.Containers[i] = c
+				break
+			}
+		}
+	}
+	return nil
+}
+
+// Cleanup cleans up user passed spec, e.g. defaulting, transforming fields.
+// TODO: move this to admission controller
+func Cleanup(c *tfv1.TfJobSpec) {
+	// TODO(jlewi): Add logic to cleanup user provided spec; e.g. by filling in defaults.
+	// We should have default container images so user doesn't have to provide these.
+}
+
+func CRDName() string {
+	return fmt.Sprintf("%s.%s", tfv1.CRDKindPlural, tfv1.CRDGroup)
+}
+
+func scalingReason(from, to int) string {
+	return fmt.Sprintf("Current cluster size: %d, desired cluster size: %d", from, to)
+}
---
+++ pkg/apis/tensorflow/v1alpha1/defaults.go
@@ -0,0 +1,74 @@
+package v1alpha1
+
+import (
+	"github.com/golang/protobuf/proto"
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+
+func addDefaultingFuncs(scheme *runtime.Scheme) error {
+	return RegisterDefaults(scheme)
+}
+
+
+// SetDefaults_TfJob sets any unspecified values to defaults
+func SetDefaults_TfJob(obj *TfJob) {
+	c := obj.Spec
+
+	if c.TfImage == "" {
+		c.TfImage = DefaultTFImage
+	}
+
+	// Check that each replica has a TensorFlow container.
+	for _, r := range c.ReplicaSpecs {
+
+		if r.TfPort == nil {
+			r.TfPort = proto.Int32(TfPort)
+		}
+
+		if string(r.TfReplicaType) == "" {
+			r.TfReplicaType = MASTER
+		}
+
+		if r.Replicas == nil {
+			r.Replicas = proto.Int32(Replicas)
+		}
+
+		//Set the default configuration for a PS server if the user didn't specify a PodTemplateSpec
+		if r.Template == nil && r.TfReplicaType == PS {
+			setDefault_PSPodTemplateSpec(r, c.TfImage)
+		}
+	}
+	if c.TerminationPolicy == nil {
+		c.TerminationPolicy = &TerminationPolicySpec{
+			Chief: &ChiefSpec{
+				ReplicaName:  "MASTER",
+				ReplicaIndex: 0,
+			},
+		}
+	}
+
+}
+
+func setDefault_PSPodTemplateSpec(r *TfReplicaSpec, tfImage string) {
+	r.IsDefaultPS = true
+	r.Template = &v1.PodTemplateSpec{
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				v1.Container{
+					Image: tfImage,
+					Name:  "tensorflow",
+					VolumeMounts: []v1.VolumeMount{
+						v1.VolumeMount{
+							Name:      "ps-config-volume",
+							MountPath: "/ps-server",
+						},
+					},
+				},
+			},
+			RestartPolicy: v1.RestartPolicyOnFailure,
+		},
+	}
+}
+
