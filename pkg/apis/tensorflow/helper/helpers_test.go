ed7cae7552bacfa4d693fb8b5975d2e1ec62194a
XsWack <xushiwei5@huawei.com>
2017-12-30 00:50:42 +0800

    Refactor the TfJob to use K8s libraries  (#215)
    fix #206
    
    1.refactor the spec package to the kubernetes standard API, all detention of our CRD is in pkg pkg/apis/tensorflow and also the validation、 set Default for the CRD and some help func.
    
    2.for use K8s code gen, wo import the project https://github.com/kubernetes/code-generator, and some shell script in the folder hack. You can see how to use it in https://blog.openshift.com/kubernetes-deep-dive-code-generation-customresources/
    
    3.All the file in pkg/client are all auto generated and we should not modify it manually.
    
    4.I do not plan to change the logical to use the sharedInformer and clientset. This PR is big enough, I think I can do it in a follow PR.
    
    5.SharedInformer is a helpful tool with cache that we can use it to list and watch our resources. More details you can see http://borismattijssen.github.io/articles/kubernetes-informers-controllers-reflectors-stores

---
+++ pkg/apis/tensorflow/helper/helpers_test.go
@@ -0,0 +1,234 @@
+package helper
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/tensorflow/k8s/pkg/util"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/api/core/v1"
+	tfv1 "github.com/tensorflow/k8s/pkg/apis/tensorflow/v1alpha1"
+)
+
+func TestAddAccelertor(t *testing.T) {
+	type testCase struct {
+		in       *tfv1.TfJobSpec
+		expected *tfv1.TfJobSpec
+		config   map[string]tfv1.AcceleratorConfig
+	}
+
+	testCases := []testCase{
+		// Case 1 checks that we look at requests.
+		{
+			in: &tfv1.TfJobSpec{
+				ReplicaSpecs: []*tfv1.TfReplicaSpec{
+					{
+						Replicas: proto.Int32(2),
+						TfPort:   proto.Int32(10),
+						Template: &v1.PodTemplateSpec{
+							Spec: v1.PodSpec{
+								Containers: []v1.Container{
+									{
+										Name: "tensorflow",
+										Resources: v1.ResourceRequirements{
+											Requests: map[v1.ResourceName]resource.Quantity{
+												"nvidia-gpu": resource.MustParse("1"),
+											},
+										},
+									},
+								},
+							},
+						},
+						TfReplicaType: tfv1.PS,
+					},
+				},
+			},
+			expected: &tfv1.TfJobSpec{
+				ReplicaSpecs: []*tfv1.TfReplicaSpec{
+					{
+						Replicas: proto.Int32(2),
+						TfPort:   proto.Int32(10),
+						Template: &v1.PodTemplateSpec{
+							Spec: v1.PodSpec{
+								Containers: []v1.Container{
+									{
+										Name: "tensorflow",
+										Resources: v1.ResourceRequirements{
+											Requests: map[v1.ResourceName]resource.Quantity{
+												"nvidia-gpu": resource.MustParse("1"),
+											},
+										},
+										VolumeMounts: []v1.VolumeMount{
+											{
+												Name:      "cuda-lib",
+												MountPath: "/usr/local/cuda",
+											},
+										},
+									},
+								},
+								Volumes: []v1.Volume{
+									{
+										Name: "cuda-lib",
+										VolumeSource: v1.VolumeSource{
+											HostPath: &v1.HostPathVolumeSource{
+												Path: "/home/cuda",
+											},
+										},
+									},
+								},
+							},
+						},
+						TfReplicaType: tfv1.PS,
+					},
+				},
+			},
+			config: map[string]tfv1.AcceleratorConfig{
+				"nvidia-gpu": tfv1.AcceleratorConfig{
+					Volumes: []tfv1.AcceleratorVolume{
+						{
+							Name:      "cuda-lib",
+							HostPath:  "/home/cuda",
+							MountPath: "/usr/local/cuda",
+						},
+					},
+				},
+			},
+		},
+		// Case 2 checks that we look at limit.
+		{
+			in: &tfv1.TfJobSpec{
+				ReplicaSpecs: []*tfv1.TfReplicaSpec{
+					{
+						Replicas: proto.Int32(2),
+						TfPort:   proto.Int32(10),
+						Template: &v1.PodTemplateSpec{
+							Spec: v1.PodSpec{
+								Containers: []v1.Container{
+									{
+										Name: "tensorflow",
+										Resources: v1.ResourceRequirements{
+											Limits: map[v1.ResourceName]resource.Quantity{
+												"nvidia-gpu": resource.MustParse("1"),
+											},
+										},
+									},
+								},
+							},
+						},
+						TfReplicaType: tfv1.PS,
+					},
+				},
+			},
+			expected: &tfv1.TfJobSpec{
+				ReplicaSpecs: []*tfv1.TfReplicaSpec{
+					{
+						Replicas: proto.Int32(2),
+						TfPort:   proto.Int32(10),
+						Template: &v1.PodTemplateSpec{
+							Spec: v1.PodSpec{
+								Containers: []v1.Container{
+									{
+										Name: "tensorflow",
+										Resources: v1.ResourceRequirements{
+											Limits: map[v1.ResourceName]resource.Quantity{
+												"nvidia-gpu": resource.MustParse("1"),
+											},
+										},
+										VolumeMounts: []v1.VolumeMount{
+											{
+												Name:      "cuda-lib",
+												MountPath: "/usr/local/cuda",
+											},
+										},
+									},
+								},
+								Volumes: []v1.Volume{
+									{
+										Name: "cuda-lib",
+										VolumeSource: v1.VolumeSource{
+											HostPath: &v1.HostPathVolumeSource{
+												Path: "/home/cuda",
+											},
+										},
+									},
+								},
+							},
+						},
+						TfReplicaType: tfv1.PS,
+					},
+				},
+			},
+			config: map[string]tfv1.AcceleratorConfig{
+				"nvidia-gpu": tfv1.AcceleratorConfig{
+					Volumes: []tfv1.AcceleratorVolume{
+						{
+							Name:      "cuda-lib",
+							HostPath:  "/home/cuda",
+							MountPath: "/usr/local/cuda",
+						},
+					},
+				},
+			},
+		},
+		// Case 3 no GPUs
+		{
+			in: &tfv1.TfJobSpec{
+				ReplicaSpecs: []*tfv1.TfReplicaSpec{
+					{
+						Replicas: proto.Int32(2),
+						TfPort:   proto.Int32(10),
+						Template: &v1.PodTemplateSpec{
+							Spec: v1.PodSpec{
+								Containers: []v1.Container{
+									{
+										Name: "tensorflow",
+									},
+								},
+							},
+						},
+						TfReplicaType: tfv1.PS,
+					},
+				},
+			},
+			expected: &tfv1.TfJobSpec{
+				ReplicaSpecs: []*tfv1.TfReplicaSpec{
+					{
+						Replicas: proto.Int32(2),
+						TfPort:   proto.Int32(10),
+						Template: &v1.PodTemplateSpec{
+							Spec: v1.PodSpec{
+								Containers: []v1.Container{
+									{
+										Name: "tensorflow",
+									},
+								},
+							},
+						},
+						TfReplicaType: tfv1.PS,
+					},
+				},
+			},
+			config: map[string]tfv1.AcceleratorConfig{
+				"nvidia-gpu": tfv1.AcceleratorConfig{
+					Volumes: []tfv1.AcceleratorVolume{
+						{
+							Name:      "cuda-lib",
+							HostPath:  "/home/cuda",
+							MountPath: "/usr/local/cuda",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, c := range testCases {
+		if err := ConfigureAcceleratorsForTfJobSpec(c.in, c.config); err != nil {
+			t.Errorf("ConfigureAccelerators error; %v", err)
+		}
+		if !reflect.DeepEqual(c.in, c.expected) {
+			t.Errorf("Want\n%v; Got\n %v", util.Pformat(c.expected), util.Pformat(c.in))
+		}
+	}
+}
---
+++ pkg/client/clientset/versioned/typed/tensorflow/v1alpha1/tfjob.go
@@ -0,0 +1,155 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	v1alpha1 "github.com/tensorflow/k8s/pkg/apis/tensorflow/v1alpha1"
+	scheme "github.com/tensorflow/k8s/pkg/client/clientset/versioned/scheme"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	types "k8s.io/apimachinery/pkg/types"
+	watch "k8s.io/apimachinery/pkg/watch"
+	rest "k8s.io/client-go/rest"
+)
+
+// TfJobsGetter has a method to return a TfJobInterface.
+// A group's client should implement this interface.
+type TfJobsGetter interface {
+	TfJobs(namespace string) TfJobInterface
+}
+
+// TfJobInterface has methods to work with TfJob resources.
+type TfJobInterface interface {
+	Create(*v1alpha1.TfJob) (*v1alpha1.TfJob, error)
+	Update(*v1alpha1.TfJob) (*v1alpha1.TfJob, error)
+	Delete(name string, options *v1.DeleteOptions) error
+	DeleteCollection(options *v1.DeleteOptions, listOptions v1.ListOptions) error
+	Get(name string, options v1.GetOptions) (*v1alpha1.TfJob, error)
+	List(opts v1.ListOptions) (*v1alpha1.TfJobList, error)
+	Watch(opts v1.ListOptions) (watch.Interface, error)
+	Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.TfJob, err error)
+	TfJobExpansion
+}
+
+// tfJobs implements TfJobInterface
+type tfJobs struct {
+	client rest.Interface
+	ns     string
+}
+
+// newTfJobs returns a TfJobs
+func newTfJobs(c *TensorflowV1alpha1Client, namespace string) *tfJobs {
+	return &tfJobs{
+		client: c.RESTClient(),
+		ns:     namespace,
+	}
+}
+
+// Get takes name of the tfJob, and returns the corresponding tfJob object, and an error if there is any.
+func (c *tfJobs) Get(name string, options v1.GetOptions) (result *v1alpha1.TfJob, err error) {
+	result = &v1alpha1.TfJob{}
+	err = c.client.Get().
+		Namespace(c.ns).
+		Resource("tfjobs").
+		Name(name).
+		VersionedParams(&options, scheme.ParameterCodec).
+		Do().
+		Into(result)
+	return
+}
+
+// List takes label and field selectors, and returns the list of TfJobs that match those selectors.
+func (c *tfJobs) List(opts v1.ListOptions) (result *v1alpha1.TfJobList, err error) {
+	result = &v1alpha1.TfJobList{}
+	err = c.client.Get().
+		Namespace(c.ns).
+		Resource("tfjobs").
+		VersionedParams(&opts, scheme.ParameterCodec).
+		Do().
+		Into(result)
+	return
+}
+
+// Watch returns a watch.Interface that watches the requested tfJobs.
+func (c *tfJobs) Watch(opts v1.ListOptions) (watch.Interface, error) {
+	opts.Watch = true
+	return c.client.Get().
+		Namespace(c.ns).
+		Resource("tfjobs").
+		VersionedParams(&opts, scheme.ParameterCodec).
+		Watch()
+}
+
+// Create takes the representation of a tfJob and creates it.  Returns the server's representation of the tfJob, and an error, if there is any.
+func (c *tfJobs) Create(tfJob *v1alpha1.TfJob) (result *v1alpha1.TfJob, err error) {
+	result = &v1alpha1.TfJob{}
+	err = c.client.Post().
+		Namespace(c.ns).
+		Resource("tfjobs").
+		Body(tfJob).
+		Do().
+		Into(result)
+	return
+}
+
+// Update takes the representation of a tfJob and updates it. Returns the server's representation of the tfJob, and an error, if there is any.
+func (c *tfJobs) Update(tfJob *v1alpha1.TfJob) (result *v1alpha1.TfJob, err error) {
+	result = &v1alpha1.TfJob{}
+	err = c.client.Put().
+		Namespace(c.ns).
+		Resource("tfjobs").
+		Name(tfJob.Name).
+		Body(tfJob).
+		Do().
+		Into(result)
+	return
+}
+
+// Delete takes name of the tfJob and deletes it. Returns an error if one occurs.
+func (c *tfJobs) Delete(name string, options *v1.DeleteOptions) error {
+	return c.client.Delete().
+		Namespace(c.ns).
+		Resource("tfjobs").
+		Name(name).
+		Body(options).
+		Do().
+		Error()
+}
+
+// DeleteCollection deletes a collection of objects.
+func (c *tfJobs) DeleteCollection(options *v1.DeleteOptions, listOptions v1.ListOptions) error {
+	return c.client.Delete().
+		Namespace(c.ns).
+		Resource("tfjobs").
+		VersionedParams(&listOptions, scheme.ParameterCodec).
+		Body(options).
+		Do().
+		Error()
+}
+
+// Patch applies the patch and returns the patched tfJob.
+func (c *tfJobs) Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.TfJob, err error) {
+	result = &v1alpha1.TfJob{}
+	err = c.client.Patch(pt).
+		Namespace(c.ns).
+		Resource("tfjobs").
+		SubResource(subresources...).
+		Name(name).
+		Body(data).
+		Do().
+		Into(result)
+	return
+}
