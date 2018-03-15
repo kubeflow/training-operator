e559cea720054438a23e928e97d829705b8d4211
Penghao Cen <cenph@caicloud.io>
2018-03-15 11:50:59 +0800

    Add options kubeconfig/master/threadiness and remove deprecated options

--- cmd/tf-operator/app/options/options.go
+++ cmd/tf-operator/app/options/options.go
@@ -16,17 +16,15 @@ package options
 
 import (
 	"flag"
-	"time"
 )
 
 // ServerOption is the main context object for the controller manager.
 type ServerOption struct {
-	ChaosLevel           int
-	ControllerConfigFile string
-	PrintVersion         bool
-	GCInterval           time.Duration
-	JsonLogFormat        bool
-	EnableGangScheduling bool
+	Kubeconfig    string
+	MasterURL     string
+	Threadiness   int
+	PrintVersion  bool
+	JSONLogFormat bool
 }
 
 // NewServerOption creates a new CMServer with a default config.
@@ -35,13 +33,20 @@ func NewServerOption() *ServerOption {
 	return &s
 }
 
-// AddFlags adds flags for a specific CMServer to the specified FlagSet
+// AddFlags adds flags for a specific CMServer to the specified FlagSet.
 func (s *ServerOption) AddFlags(fs *flag.FlagSet) {
-	// chaos level will be removed once we have a formal tool to inject failures.
-	fs.IntVar(&s.ChaosLevel, "chaos-level", -1, "DO NOT USE IN PRODUCTION - level of chaos injected into the TFJob created by the operator.")
+	fs.StringVar(&s.Kubeconfig, "kubeconfig", "~/.kube/config",
+		`Path to a kubeconfig, only required if out-of-cluster.`)
+
+	fs.StringVar(&s.MasterURL, "master", "",
+		`The url of the Kubernetes API server,
+		 will overrides any value in kubeconfig, only required if out-of-cluster.`)
+
+	fs.IntVar(&s.Threadiness, "threadiness", 2,
+		`How many threads to process the main logic`)
+
 	fs.BoolVar(&s.PrintVersion, "version", false, "Show version and quit")
-	fs.DurationVar(&s.GCInterval, "gc-interval", 10*time.Minute, "GC interval")
-	fs.StringVar(&s.ControllerConfigFile, "controller-config-file", "", "Path to file containing the controller config.")
-	fs.BoolVar(&s.JsonLogFormat, "json-log-format", true, "Set true to use json style log format. Set false to use plaintext style log format")
-	fs.BoolVar(&s.EnableGangScheduling, "enable-gang-scheduling", false, "Set true to enable gang scheduling by kube-arbitrator.")
+
+	fs.BoolVar(&s.JSONLogFormat, "json-log-format", true,
+		"Set true to use json style log format. Set false to use plaintext style log format")
 }
