924e927952da5ec7ef0b3d6c1700155c85332410
Ce Gao <ce.gao@outlook.com>
2018-06-01 14:57:58 +0800

    test: Add test cases for service ref manager and control interface (#615)
    * vendor: Update
    
    Signed-off-by: Ce Gao <gaoce@caicloud.io>
    
    * test: Add
    
    Signed-off-by: Ce Gao <gaoce@caicloud.io>
    
    * test: Add ref manager test
    
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

---
+++ pkg/controller.v2/service_control_test.go
@@ -0,0 +1,79 @@
+// Copyright 2018 The Kubeflow Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package controller
+
+import (
+	"encoding/json"
+	"net/http/httptest"
+	"testing"
+
+	"k8s.io/api/core/v1"
+	apiequality "k8s.io/apimachinery/pkg/api/equality"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientset "k8s.io/client-go/kubernetes"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/record"
+	utiltesting "k8s.io/client-go/util/testing"
+	"k8s.io/kubernetes/pkg/api/testapi"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateService(t *testing.T) {
+	ns := metav1.NamespaceDefault
+	body := runtime.EncodeOrDie(testapi.Default.Codec(), &v1.Service{ObjectMeta: metav1.ObjectMeta{Name: "empty_service"}})
+	fakeHandler := utiltesting.FakeHandler{
+		StatusCode:   200,
+		ResponseBody: string(body),
+	}
+	testServer := httptest.NewServer(&fakeHandler)
+	defer testServer.Close()
+	clientset := clientset.NewForConfigOrDie(&restclient.Config{
+		Host: testServer.URL,
+		ContentConfig: restclient.ContentConfig{
+			GroupVersion: &v1.SchemeGroupVersion,
+		},
+	})
+
+	serviceControl := RealServiceControl{
+		KubeClient: clientset,
+		Recorder:   &record.FakeRecorder{},
+	}
+
+	tfJob := newTFJob(1, 0)
+
+	testName := "service-name"
+	service := newBaseService(testName, tfJob, t)
+
+	// Make sure createReplica sends a POST to the apiserver with a pod from the controllers pod template
+	err := serviceControl.CreateServices(ns, service, tfJob)
+	assert.NoError(t, err, "unexpected error: %v", err)
+
+	expectedService := v1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels:          genLabels(getKey(tfJob, t)),
+			Name:            testName,
+			Namespace:       ns,
+			OwnerReferences: []metav1.OwnerReference{*metav1.NewControllerRef(tfJob, controllerKind)},
+		},
+	}
+	fakeHandler.ValidateRequest(t, testapi.Default.ResourcePath("services", metav1.NamespaceDefault, ""), "POST", nil)
+	var actualService = &v1.Service{}
+	err = json.Unmarshal([]byte(fakeHandler.RequestBody), actualService)
+	assert.NoError(t, err, "unexpected error: %v", err)
+	assert.True(t, apiequality.Semantic.DeepDerivative(&expectedService, actualService),
+		"Body: %s", fakeHandler.RequestBody)
+}
---
+++ pkg/controller.v2/service_ref_manager_test.go
@@ -0,0 +1,199 @@
+// Copyright 2018 The Kubeflow Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package controller
+
+import (
+	"reflect"
+	"testing"
+
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+func TestClaimServices(t *testing.T) {
+	controllerUID := "123"
+
+	type test struct {
+		name     string
+		manager  *ServiceControllerRefManager
+		services []*v1.Service
+		filters  []func(*v1.Service) bool
+		claimed  []*v1.Service
+		released []*v1.Service
+	}
+	var tests = []test{
+		func() test {
+			tfJob := newTFJob(1, 0)
+			tfJobLabelSelector, err := metav1.LabelSelectorAsSelector(&metav1.LabelSelector{
+				MatchLabels: genLabels(getKey(tfJob, t)),
+			})
+			if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+			}
+			testService := newBaseService("service2", tfJob, nil)
+			testService.Labels[labelGroupName] = "testing"
+
+			return test{
+				name: "Claim services with correct label",
+				manager: NewServiceControllerRefManager(&FakeServiceControl{},
+					tfJob,
+					tfJobLabelSelector,
+					controllerKind,
+					func() error { return nil }),
+				services: []*v1.Service{newBaseService("service1", tfJob, t), testService},
+				claimed:  []*v1.Service{newBaseService("service1", tfJob, t)},
+			}
+		}(),
+		func() test {
+			controller := newTFJob(1, 0)
+			controllerLabelSelector, err := metav1.LabelSelectorAsSelector(&metav1.LabelSelector{
+				MatchLabels: genLabels(getKey(controller, t)),
+			})
+			if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+			}
+			controller.UID = types.UID(controllerUID)
+			now := metav1.Now()
+			controller.DeletionTimestamp = &now
+			testService1 := newBaseService("service1", controller, t)
+			testService1.SetOwnerReferences([]metav1.OwnerReference{})
+			testService2 := newBaseService("service2", controller, t)
+			testService2.SetOwnerReferences([]metav1.OwnerReference{})
+			return test{
+				name: "Controller marked for deletion can not claim services",
+				manager: NewServiceControllerRefManager(&FakeServiceControl{},
+					controller,
+					controllerLabelSelector,
+					controllerKind,
+					func() error { return nil }),
+				services: []*v1.Service{testService1, testService2},
+				claimed:  nil,
+			}
+		}(),
+		func() test {
+			controller := newTFJob(1, 0)
+			controllerLabelSelector, err := metav1.LabelSelectorAsSelector(&metav1.LabelSelector{
+				MatchLabels: genLabels(getKey(controller, t)),
+			})
+			if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+			}
+			controller.UID = types.UID(controllerUID)
+			now := metav1.Now()
+			controller.DeletionTimestamp = &now
+			testService2 := newBaseService("service2", controller, t)
+			testService2.SetOwnerReferences([]metav1.OwnerReference{})
+			return test{
+				name: "Controller marked for deletion can not claim new services",
+				manager: NewServiceControllerRefManager(&FakeServiceControl{},
+					controller,
+					controllerLabelSelector,
+					controllerKind,
+					func() error { return nil }),
+				services: []*v1.Service{newBaseService("service1", controller, t), testService2},
+				claimed:  []*v1.Service{newBaseService("service1", controller, t)},
+			}
+		}(),
+		func() test {
+			controller := newTFJob(1, 0)
+			controllerLabelSelector, err := metav1.LabelSelectorAsSelector(&metav1.LabelSelector{
+				MatchLabels: genLabels(getKey(controller, t)),
+			})
+			if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+			}
+			controller2 := newTFJob(1, 0)
+			controller.UID = types.UID(controllerUID)
+			controller2.UID = types.UID("AAAAA")
+			return test{
+				name: "Controller can not claim services owned by another controller",
+				manager: NewServiceControllerRefManager(&FakeServiceControl{},
+					controller,
+					controllerLabelSelector,
+					controllerKind,
+					func() error { return nil }),
+				services: []*v1.Service{newBaseService("service1", controller, t), newBaseService("service2", controller2, t)},
+				claimed:  []*v1.Service{newBaseService("service1", controller, t)},
+			}
+		}(),
+		func() test {
+			controller := newTFJob(1, 0)
+			controllerLabelSelector, err := metav1.LabelSelectorAsSelector(&metav1.LabelSelector{
+				MatchLabels: genLabels(getKey(controller, t)),
+			})
+			if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+			}
+			controller.UID = types.UID(controllerUID)
+			testService2 := newBaseService("service2", controller, t)
+			testService2.Labels[labelGroupName] = "testing"
+			return test{
+				name: "Controller releases claimed services when selector doesn't match",
+				manager: NewServiceControllerRefManager(&FakeServiceControl{},
+					controller,
+					controllerLabelSelector,
+					controllerKind,
+					func() error { return nil }),
+				services: []*v1.Service{newBaseService("service1", controller, t), testService2},
+				claimed:  []*v1.Service{newBaseService("service1", controller, t)},
+			}
+		}(),
+		func() test {
+			controller := newTFJob(1, 0)
+			controllerLabelSelector, err := metav1.LabelSelectorAsSelector(&metav1.LabelSelector{
+				MatchLabels: genLabels(getKey(controller, t)),
+			})
+			if err != nil {
+				t.Errorf("Unexpected error: %v", err)
+			}
+			controller.UID = types.UID(controllerUID)
+			testService1 := newBaseService("service1", controller, t)
+			testService2 := newBaseService("service2", controller, t)
+			testService2.Labels[labelGroupName] = "testing"
+			now := metav1.Now()
+			testService1.DeletionTimestamp = &now
+			testService2.DeletionTimestamp = &now
+
+			return test{
+				name: "Controller does not claim orphaned services marked for deletion",
+				manager: NewServiceControllerRefManager(&FakeServiceControl{},
+					controller,
+					controllerLabelSelector,
+					controllerKind,
+					func() error { return nil }),
+				services: []*v1.Service{testService1, testService2},
+				claimed:  []*v1.Service{testService1},
+			}
+		}(),
+	}
+	for _, test := range tests {
+		claimed, err := test.manager.ClaimServices(test.services)
+		if err != nil {
+			t.Errorf("Test case `%s`, unexpected error: %v", test.name, err)
+		} else if !reflect.DeepEqual(test.claimed, claimed) {
+			t.Errorf("Test case `%s`, claimed wrong services. Expected %v, got %v", test.name, serviceToStringSlice(test.claimed), serviceToStringSlice(claimed))
+		}
+
+	}
+}
+
+func serviceToStringSlice(services []*v1.Service) []string {
+	var names []string
+	for _, service := range services {
+		names = append(names, service.Name)
+	}
+	return names
+}
