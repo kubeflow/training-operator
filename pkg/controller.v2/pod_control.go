4b1d7df66a728f2883cb45d278bd5ed476096004
Ce Gao <gaoce@caicloud.io>
2018-06-13 18:13:55 +0800

    pod: Add test for exit code
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- pkg/controller.v2/pod_control.go
+++ pkg/controller.v2/pod_control.go
@@ -21,7 +21,6 @@ package controller
 
 import (
 	"fmt"
-	"sync"
 
 	"github.com/golang/glog"
 	"k8s.io/api/core/v1"
@@ -163,92 +162,4 @@ func (r RealPodControl) DeletePod(namespace string, podID string, object runtime
 		r.Recorder.Eventf(object, v1.EventTypeNormal, SuccessfulDeletePodReason, "Deleted pod: %v", podID)
 	}
 	return nil
-}
-
-type FakePodControl struct {
-	sync.Mutex
-	Templates       []v1.PodTemplateSpec
-	ControllerRefs  []metav1.OwnerReference
-	DeletePodName   []string
-	Patches         [][]byte
-	Err             error
-	CreateLimit     int
-	CreateCallCount int
-}
-
-var _ controller.PodControlInterface = &FakePodControl{}
-
-func (f *FakePodControl) PatchPod(namespace, name string, data []byte) error {
-	f.Lock()
-	defer f.Unlock()
-	f.Patches = append(f.Patches, data)
-	if f.Err != nil {
-		return f.Err
-	}
-	return nil
-}
-
-func (f *FakePodControl) CreatePods(namespace string, spec *v1.PodTemplateSpec, object runtime.Object) error {
-	f.Lock()
-	defer f.Unlock()
-	f.CreateCallCount++
-	if f.CreateLimit != 0 && f.CreateCallCount > f.CreateLimit {
-		return fmt.Errorf("Not creating pod, limit %d already reached (create call %d)", f.CreateLimit, f.CreateCallCount)
-	}
-	f.Templates = append(f.Templates, *spec)
-	if f.Err != nil {
-		return f.Err
-	}
-	return nil
-}
-
-func (f *FakePodControl) CreatePodsWithControllerRef(namespace string, spec *v1.PodTemplateSpec, object runtime.Object, controllerRef *metav1.OwnerReference) error {
-	f.Lock()
-	defer f.Unlock()
-	f.CreateCallCount++
-	if f.CreateLimit != 0 && f.CreateCallCount > f.CreateLimit {
-		return fmt.Errorf("Not creating pod, limit %d already reached (create call %d)", f.CreateLimit, f.CreateCallCount)
-	}
-	f.Templates = append(f.Templates, *spec)
-	f.ControllerRefs = append(f.ControllerRefs, *controllerRef)
-	if f.Err != nil {
-		return f.Err
-	}
-	return nil
-}
-
-func (f *FakePodControl) CreatePodsOnNode(nodeName, namespace string, template *v1.PodTemplateSpec, object runtime.Object, controllerRef *metav1.OwnerReference) error {
-	f.Lock()
-	defer f.Unlock()
-	f.CreateCallCount++
-	if f.CreateLimit != 0 && f.CreateCallCount > f.CreateLimit {
-		return fmt.Errorf("Not creating pod, limit %d already reached (create call %d)", f.CreateLimit, f.CreateCallCount)
-	}
-	f.Templates = append(f.Templates, *template)
-	f.ControllerRefs = append(f.ControllerRefs, *controllerRef)
-	if f.Err != nil {
-		return f.Err
-	}
-	return nil
-}
-
-func (f *FakePodControl) DeletePod(namespace string, podID string, object runtime.Object) error {
-	f.Lock()
-	defer f.Unlock()
-	f.DeletePodName = append(f.DeletePodName, podID)
-	if f.Err != nil {
-		return f.Err
-	}
-	return nil
-}
-
-func (f *FakePodControl) Clear() {
-	f.Lock()
-	defer f.Unlock()
-	f.DeletePodName = []string{}
-	f.Templates = []v1.PodTemplateSpec{}
-	f.ControllerRefs = []metav1.OwnerReference{}
-	f.Patches = [][]byte{}
-	f.CreateLimit = 0
-	f.CreateCallCount = 0
 }
