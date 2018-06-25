cc3f02daf0d6de5d7862acf0b2b39b715806a419
Pengyu Chen <chenpengyu@caicloud.io>
2018-06-25 15:40:01 +0800

    check level is no need

--- pkg/control/service_control.go
+++ pkg/control/service_control.go
@@ -111,9 +111,7 @@ func (r RealServiceControl) createServices(namespace string, service *v1.Service
 		log.Errorf("parentObject does not have ObjectMeta, %v", err)
 		return nil
 	}
-	if log.GetLevel() >= log.InfoLevel {
-		log.Infof("Controller %v created service %v", accessor.GetName(), newService.Name)
-	}
+	log.Infof("Controller %v created service %v", accessor.GetName(), newService.Name)
 	r.Recorder.Eventf(object, v1.EventTypeNormal, SuccessfulCreateServiceReason, "Created service: %v", newService.Name)
 
 	return nil
@@ -125,9 +123,7 @@ func (r RealServiceControl) DeleteService(namespace, serviceID string, object ru
 	if err != nil {
 		return fmt.Errorf("object does not have ObjectMeta, %v", err)
 	}
-	if log.GetLevel() >= log.ErrorLevel {
-		log.Infof("Controller %v deleting service %v/%v", accessor.GetName(), namespace, serviceID)
-	}
+	log.Infof("Controller %v deleting service %v/%v", accessor.GetName(), namespace, serviceID)
 	if err := r.KubeClient.CoreV1().Services(namespace).Delete(serviceID, nil); err != nil {
 		r.Recorder.Eventf(object, v1.EventTypeWarning, FailedDeleteServiceReason, "Error deleting: %v", err)
 		return fmt.Errorf("unable to delete service: %v", err)
