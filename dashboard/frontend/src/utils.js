018c0447f86c9b66de0ae202772000ef697b4e45
ChanYiLin <j5111261112@gmail.com>
2018-09-05 11:58:15 +0800

    allow users to config resources while creating job

--- dashboard/frontend/src/utils.js
+++ dashboard/frontend/src/utils.js
@@ -3,7 +3,7 @@ export function getHost() {
   const pLen = prefix.length;
 
   const fullPath = window.location.pathname;
-  if (fullPath.indexOf(prefix) != -1) {
+  if (fullPath.indexOf(prefix) !== -1) {
     const keepLen = fullPath.indexOf(prefix) + pLen;
     return fullPath.substr(0, keepLen);
   } else {
