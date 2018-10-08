15a90142840e2a1ad5bc522936e20c06d1f6a62a
lovejoy <lyq.0617@gmail.com>
2018-10-08 16:46:32 +0800

    add frontend-dir & port to backend program

--- dashboard/backend/main.go
+++ dashboard/backend/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"os"
-
-	"flag"
+	"strconv"
 
 	"github.com/kubeflow/tf-operator/dashboard/backend/client"
 	"github.com/kubeflow/tf-operator/dashboard/backend/handler"
 )
 
+const (
+	DefaultFrontendDir string = "/opt/tensorflow_k8s/dashboard/frontend/build/"
+	DefaultBackendPort int    = 8080
+)
+
 func main() {
+	var frontendDir string
+	var port int
+	flag.StringVar(&frontendDir, "frontend-dir", DefaultFrontendDir,
+		`directory of the dashboard frontend`)
+	flag.IntVar(&port, "port", DefaultBackendPort,
+		`port this program will listen`)
 	flag.Parse()
 	log.SetOutput(os.Stdout)
 	cm, err := client.NewClientManager()
@@ -23,10 +34,10 @@ func main() {
 		log.Fatalf("Error while creating the API Handler: %v", err)
 	}
 
-	http.Handle("/tfjobs/ui/", http.StripPrefix("/tfjobs/ui/", http.FileServer(http.Dir("/opt/tensorflow_k8s/dashboard/frontend/build/"))))
+	http.Handle("/tfjobs/ui/", http.StripPrefix("/tfjobs/ui/", http.FileServer(http.Dir(frontendDir))))
 	http.Handle("/tfjobs/api/", apiHandler)
 
-	p := ":8080"
+	p := ":" + strconv.Itoa(port)
 	log.Println("Dashboard available at /tfjobs/ui/ on port", p)
 
 	if err = http.ListenAndServe(p, nil); err != nil {
