3b02688323eeed25655404bc36fbd149c2bd4a7f
xushiwei 00425595 <xushiwei5@huawei.com>
2019-05-27 20:44:55 +0800

    Add pending status for pastBackoffLimitOnFailure

--- pkg/controller.v1/tensorflow/controller.go
+++ pkg/controller.v1/tensorflow/controller.go
@@ -510,16 +510,15 @@ func (tc *TFController) pastBackoffLimit(tfjob *tfv1.TFJob, pods []*v1.Pod) (boo
 		}
 		for i := range pods {
 			po := pods[i]
-			if po.Status.Phase != v1.PodRunning {
-				continue
-			}
-			for j := range po.Status.InitContainerStatuses {
-				stat := po.Status.InitContainerStatuses[j]
-				result += stat.RestartCount
-			}
-			for j := range po.Status.ContainerStatuses {
-				stat := po.Status.ContainerStatuses[j]
-				result += stat.RestartCount
+			if po.Status.Phase == v1.PodRunning || po.Status.Phase != v1.PodPending {
+				for j := range po.Status.InitContainerStatuses {
+					stat := po.Status.InitContainerStatuses[j]
+					result += stat.RestartCount
+				}
+				for j := range po.Status.ContainerStatuses {
+					stat := po.Status.ContainerStatuses[j]
+					result += stat.RestartCount
+				}
 			}
 		}
 	}
