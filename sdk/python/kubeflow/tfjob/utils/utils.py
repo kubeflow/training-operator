363a0d41f3dce2dff9337e47c741c519a3c3d65c
Jin Chi He <hejinchi@cn.ibm.com>
2019-12-04 17:47:27 +0800

    generate TFJob Python SDK

---
+++ sdk/python/kubeflow/tfjob/utils/utils.py
@@ -0,0 +1,35 @@
+# Copyright 2019 kubeflow.org.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+def is_running_in_k8s():
+  return os.path.isdir('/var/run/secrets/kubernetes.io/')
+
+
+def get_current_k8s_namespace():
+  with open('/var/run/secrets/kubernetes.io/serviceaccount/namespace', 'r') as f:
+    return f.readline()
+
+
+def get_default_target_namespace():
+  if not is_running_in_k8s():
+    return 'default'
+  return get_current_k8s_namespace()
+
+
+def set_tfjob_namespace(tfjob):
+  tfjob_namespace = tfjob.metadata.namespace
+  namespace = tfjob_namespace or get_default_target_namespace()
+  return namespace
