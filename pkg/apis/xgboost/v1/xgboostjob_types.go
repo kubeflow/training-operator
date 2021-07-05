e26fcc51bbb4bc2df5b8c6b6a55197bcfc4bc0d2
Wang Zhang <zw199006@gmail.com>
2021-07-05 10:18:21 +0800

    Add XGBoost API (#1286)

---
+++ pkg/apis/xgboost/v1/xgboostjob_types.go
@@ -0,0 +1,79 @@
+// Copyright YEAR The Kubeflow Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1
+
+import (
+	commonv1 "github.com/kubeflow/common/pkg/apis/common/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// XGBoostJobSpec defines the desired state of XGBoostJob
+type XGBoostJobSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	RunPolicy commonv1.RunPolicy `json:",inline"`
+
+	XGBReplicaSpecs map[commonv1.ReplicaType]*commonv1.ReplicaSpec `json:"xgbReplicaSpecs"`
+}
+
+// XGBoostJobStatus defines the observed state of XGBoostJob
+type XGBoostJobStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	commonv1.JobStatus `json:",inline"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// XGBoostJob is the Schema for the xgboostjobs API
+// +k8s:openapi-gen=true
+type XGBoostJob struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   XGBoostJobSpec   `json:"spec,omitempty"`
+	Status XGBoostJobStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// XGBoostJobList contains a list of XGBoostJob
+type XGBoostJobList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []XGBoostJob `json:"items"`
+}
+
+// XGBoostJobReplicaType is the type for XGBoostJobReplica.
+type XGBoostJobReplicaType commonv1.ReplicaType
+
+const (
+	// XGBoostReplicaTypeMaster is the type for master replica.
+	XGBoostReplicaTypeMaster XGBoostJobReplicaType = "Master"
+
+	// XGBoostReplicaTypeWorker is the type for worker replicas.
+	XGBoostReplicaTypeWorker XGBoostJobReplicaType = "Worker"
+)
+
+func init() {
+	SchemeBuilder.Register(&XGBoostJob{}, &XGBoostJobList{})
+}
