ce0cbbad58e0358f976babb35f3dd5b5a5a7aea7
Byron wang <stormlandon@outlook.com>
2022-06-11 22:40:04 +0800

    regenerate python sdk

--- sdk/python/kubeflow/training/__init__.py
+++ sdk/python/kubeflow/training/__init__.py
@@ -27,29 +27,12 @@ from kubeflow.training.exceptions import ApiValueError
 from kubeflow.training.exceptions import ApiKeyError
 from kubeflow.training.exceptions import ApiException
 # import models into sdk package
-from kubeflow.training.models.v1_elastic_policy import V1ElasticPolicy
 from kubeflow.training.models.v1_job_condition import V1JobCondition
 from kubeflow.training.models.v1_job_status import V1JobStatus
-from kubeflow.training.models.v1_mpi_job import V1MPIJob
-from kubeflow.training.models.v1_mpi_job_list import V1MPIJobList
-from kubeflow.training.models.v1_mpi_job_spec import V1MPIJobSpec
-from kubeflow.training.models.v1_mx_job import V1MXJob
-from kubeflow.training.models.v1_mx_job_list import V1MXJobList
-from kubeflow.training.models.v1_mx_job_spec import V1MXJobSpec
-from kubeflow.training.models.v1_py_torch_job import V1PyTorchJob
-from kubeflow.training.models.v1_py_torch_job_list import V1PyTorchJobList
-from kubeflow.training.models.v1_py_torch_job_spec import V1PyTorchJobSpec
-from kubeflow.training.models.v1_rdzv_conf import V1RDZVConf
 from kubeflow.training.models.v1_replica_spec import V1ReplicaSpec
 from kubeflow.training.models.v1_replica_status import V1ReplicaStatus
 from kubeflow.training.models.v1_run_policy import V1RunPolicy
 from kubeflow.training.models.v1_scheduling_policy import V1SchedulingPolicy
-from kubeflow.training.models.v1_tf_job import V1TFJob
-from kubeflow.training.models.v1_tf_job_list import V1TFJobList
-from kubeflow.training.models.v1_tf_job_spec import V1TFJobSpec
-from kubeflow.training.models.v1_xg_boost_job import V1XGBoostJob
-from kubeflow.training.models.v1_xg_boost_job_list import V1XGBoostJobList
-from kubeflow.training.models.v1_xg_boost_job_spec import V1XGBoostJobSpec
 
 from kubeflow.training.api.tf_job_client import TFJobClient
 from kubeflow.training.api.py_torch_job_client import PyTorchJobClient
