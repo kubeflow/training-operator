c87a4cc986fed5e477e5057275546583be01f988
Johnu George <johnugeorge109@gmail.com>
2022-06-28 21:33:30 +0530

    Upgrading sdk to 1.5.0

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -25,7 +25,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
   name='kubeflow-training',
-  version='1.4.0',
+  version='1.5.0',
   author="Kubeflow Authors",
   author_email='hejinchi@cn.ibm.com',
   license="Apache License Version 2.0",
