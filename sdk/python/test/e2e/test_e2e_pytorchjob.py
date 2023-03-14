fae97376ccd9b5a0bec207c954982afab15cc802
nagar-ajay <ajay.nagar@nutanix.com>
2023-03-14 12:09:39 +0530

    update pytorchjob e2e test to use fixtures

--- sdk/python/test/e2e/test_e2e_pytorchjob.py
+++ sdk/python/test/e2e/test_e2e_pytorchjob.py
@@ -21,7 +21,6 @@ from kubernetes.client import V1ObjectMeta
 from kubernetes.client import V1PodSpec
 from kubernetes.client import V1Container
 
-from kubeflow.training import TrainingClient
 from kubeflow.training import V1ReplicaSpec
 from kubeflow.training import KubeflowOrgV1PyTorchJob
 from kubeflow.training import KubeflowOrgV1PyTorchJobSpec
@@ -36,9 +35,7 @@ from test.e2e.constants import GANG_SCHEDULERS, NONE_GANG_SCHEDULERS
 logging.basicConfig(format="%(message)s")
 logging.getLogger().setLevel(logging.INFO)
 
-TRAINING_CLIENT = TrainingClient(config_file=os.getenv("KUBECONFIG", "~/.kube/config"))
 JOB_NAME = "pytorchjob-mnist-ci-test"
-JOB_NAMESPACE = "default"
 CONTAINER_NAME = "pytorch"
 GANG_SCHEDULER_NAME = os.getenv(TEST_GANG_SCHEDULER_NAME_ENV_KEY)
 
@@ -46,7 +43,7 @@ GANG_SCHEDULER_NAME = os.getenv(TEST_GANG_SCHEDULER_NAME_ENV_KEY)
 @pytest.mark.skipif(
     GANG_SCHEDULER_NAME in NONE_GANG_SCHEDULERS, reason="For gang-scheduling",
 )
-def test_sdk_e2e_with_gang_scheduling():
+def test_sdk_e2e_with_gang_scheduling(job_namespace, training_client):
     container = generate_container()
 
     master = V1ReplicaSpec(
@@ -67,39 +64,39 @@ def test_sdk_e2e_with_gang_scheduling():
         )),
     )
 
-    unschedulable_pytorchjob = generate_pytorchjob(master, worker, V1SchedulingPolicy(min_available=10))
-    schedulable_pytorchjob = generate_pytorchjob(master, worker, V1SchedulingPolicy(min_available=2))
+    unschedulable_pytorchjob = generate_pytorchjob(master, worker, V1SchedulingPolicy(min_available=10), job_namespace)
+    schedulable_pytorchjob = generate_pytorchjob(master, worker, V1SchedulingPolicy(min_available=2), job_namespace)
 
-    TRAINING_CLIENT.create_pytorchjob(unschedulable_pytorchjob, JOB_NAMESPACE)
+    training_client.create_pytorchjob(unschedulable_pytorchjob, job_namespace)
     logging.info(f"List of created {constants.PYTORCHJOB_KIND}s")
-    logging.info(TRAINING_CLIENT.list_pytorchjobs(JOB_NAMESPACE))
+    logging.info(training_client.list_pytorchjobs(job_namespace))
 
     verify_unschedulable_job_e2e(
-        TRAINING_CLIENT,
+        training_client,
         JOB_NAME,
-        JOB_NAMESPACE,
+        job_namespace,
         constants.PYTORCHJOB_KIND,
     )
 
-    TRAINING_CLIENT.patch_pytorchjob(schedulable_pytorchjob, JOB_NAME, JOB_NAMESPACE)
+    training_client.patch_pytorchjob(schedulable_pytorchjob, JOB_NAME, job_namespace)
     logging.info(f"List of patched {constants.PYTORCHJOB_KIND}s")
-    logging.info(TRAINING_CLIENT.list_pytorchjobs(JOB_NAMESPACE))
+    logging.info(training_client.list_pytorchjobs(job_namespace))
 
     verify_job_e2e(
-        TRAINING_CLIENT,
+        training_client,
         JOB_NAME,
-        JOB_NAMESPACE,
+        job_namespace,
         constants.PYTORCHJOB_KIND,
         CONTAINER_NAME,
     )
 
-    TRAINING_CLIENT.delete_pytorchjob(JOB_NAME, JOB_NAMESPACE)
+    training_client.delete_pytorchjob(JOB_NAME, job_namespace)
 
 
 @pytest.mark.skipif(
     GANG_SCHEDULER_NAME in GANG_SCHEDULERS, reason="For plain scheduling",
 )
-def test_sdk_e2e():
+def test_sdk_e2e(job_namespace, training_client):
     container = generate_container()
 
     master = V1ReplicaSpec(
@@ -114,32 +111,33 @@ def test_sdk_e2e():
         template=V1PodTemplateSpec(spec=V1PodSpec(containers=[container])),
     )
 
-    pytorchjob = generate_pytorchjob(master, worker)
+    pytorchjob = generate_pytorchjob(master, worker, job_namespace=job_namespace)
 
-    TRAINING_CLIENT.create_pytorchjob(pytorchjob, JOB_NAMESPACE)
+    training_client.create_pytorchjob(pytorchjob, job_namespace)
     logging.info(f"List of created {constants.PYTORCHJOB_KIND}s")
-    logging.info(TRAINING_CLIENT.list_pytorchjobs(JOB_NAMESPACE))
+    logging.info(training_client.list_pytorchjobs(job_namespace))
 
     verify_job_e2e(
-        TRAINING_CLIENT,
+        training_client,
         JOB_NAME,
-        JOB_NAMESPACE,
+        job_namespace,
         constants.PYTORCHJOB_KIND,
         CONTAINER_NAME,
     )
 
-    TRAINING_CLIENT.delete_pytorchjob(JOB_NAME, JOB_NAMESPACE)
+    training_client.delete_pytorchjob(JOB_NAME, job_namespace)
 
 
 def generate_pytorchjob(
     master: V1ReplicaSpec,
     worker: V1ReplicaSpec,
     scheduling_policy: V1SchedulingPolicy = None,
+    job_namespace: str = "default",
 ) -> KubeflowOrgV1PyTorchJob:
     return KubeflowOrgV1PyTorchJob(
         api_version="kubeflow.org/v1",
         kind="PyTorchJob",
-        metadata=V1ObjectMeta(name=JOB_NAME, namespace=JOB_NAMESPACE),
+        metadata=V1ObjectMeta(name=JOB_NAME, namespace=job_namespace),
         spec=KubeflowOrgV1PyTorchJobSpec(
             run_policy=V1RunPolicy(
                 clean_pod_policy="None",
