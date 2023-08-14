48dbbf0a8e90e52c55ec05d0f689fcbf83c6b441
Yuki Iwai <yuki.iwai.tz@gmail.com>
2023-08-14 15:14:26 +0900

    Remove klog v1 (#1886)
    Signed-off-by: Yuki Iwai <yuki.iwai.tz@gmail.com>

--- pkg/controller.v1/mpi/mpijob_controller.go
+++ pkg/controller.v1/mpi/mpijob_controller.go
@@ -37,7 +37,7 @@ import (
 	"k8s.io/client-go/informers"
 	kubeclientset "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/record"
-	"k8s.io/klog"
+	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
