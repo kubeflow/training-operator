bb1017bd0848a46e2921b0fbb664d176f3d77980
deepanker13 <deepanker.gupta@nutanix.com>
2023-11-30 12:56:26 +0530

    adding constant for init container name

--- sdk/python/kubeflow/training/constants/constants.py
+++ sdk/python/kubeflow/training/constants/constants.py
@@ -78,6 +78,7 @@ TFJOB_BASE_IMAGE_GPU = "docker.io/tensorflow/tensorflow:2.9.1-gpu"
 PYTORCHJOB_KIND = "PyTorchJob"
 PYTORCHJOB_PLURAL = "pytorchjobs"
 PYTORCHJOB_CONTAINER = "pytorch"
+PYTORCHJOB_STORAGE_CONTAINER = "pytorch-storage"
 PYTORCHJOB_REPLICA_TYPES = (REPLICA_TYPE_MASTER.lower(), REPLICA_TYPE_WORKER.lower())
 
 PYTORCHJOB_BASE_IMAGE = "docker.io/pytorch/pytorch:1.12.1-cuda11.3-cudnn8-runtime"
@@ -128,6 +129,7 @@ JOB_PARAMETERS = {
         "plural": PYTORCHJOB_PLURAL,
         "container": PYTORCHJOB_CONTAINER,
         "base_image": PYTORCHJOB_BASE_IMAGE,
+        "init_container": PYTORCHJOB_STORAGE_CONTAINER
     },
     MXJOB_KIND: {
         "model": models.KubeflowOrgV1MXJob,
