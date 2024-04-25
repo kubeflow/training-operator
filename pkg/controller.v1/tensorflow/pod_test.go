733d94972721be72dd3b31443fed1b5420ff49b6
Kelly A <kellyaa@users.noreply.github.com>
2024-04-25 17:31:03 -0400

    Remove pod creation for noretry exit code test
    Signed-off-by: Kelly A <kellyaa@users.noreply.github.com>

--- pkg/controller.v1/tensorflow/pod_test.go
+++ pkg/controller.v1/tensorflow/pod_test.go
@@ -285,23 +285,10 @@ var _ = Describe("TFJob controller", func() {
 			tfJob.Spec.TFReplicaSpecs[kubeflowv1.TFJobReplicaTypeWorker].RestartPolicy = kubeflowv1.RestartPolicyExitCode
 			Expect(testK8sClient.Create(ctx, tfJob)).Should(Succeed())
 
-			refs := []metav1.OwnerReference{
-				*reconciler.GenOwnerReference(tfJob),
-			}
-			By("creating worker Pod")
-			pod := tftestutil.NewPod(tfJob, kubeflowv1.TFJobReplicaTypeWorker, 0, refs)
-			basicLabels := reconciler.GenLabels(tfJob.GetName())
-			for k, v := range basicLabels {
-				pod.Labels[k] = v
-			}
-			pod.Spec.Containers = append(pod.Spec.Containers, corev1.Container{
-				Name:  kubeflowv1.TFJobDefaultContainerName,
-				Image: tftestutil.DummyContainerImage,
-			})
-			testK8sClient.Create(ctx, pod)
+			_ = reconciler.ReconcileJobs(tfJob, tfJob.Spec.TFReplicaSpecs, tfJob.Status, &tfJob.Spec.RunPolicy)
 
 			created := &corev1.Pod{}
-			key := types.NamespacedName{Namespace: metav1.NamespaceDefault, Name: pod.Name}
+			key := types.NamespacedName{Namespace: metav1.NamespaceDefault, Name: "test-noretry-exit-code-worker-0"}
 			Expect(testK8sClient.Get(ctx, key, created)).Should(Succeed())
 			created.Status.Phase = corev1.PodFailed
 			created.Status.ContainerStatuses = append(created.Status.ContainerStatuses, corev1.ContainerStatus{
