7989e39db9c4dd674efdd01e0207788dcd3fda2c
Andrey Velichkevich <andrey.velichkevich@gmail.com>
2024-11-29 00:21:12 +0000

    Remove old gen_openapi
    Signed-off-by: Andrey Velichkevich <andrey.velichkevich@gmail.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -56,26 +56,6 @@ go run ${OPENAPI_PKG}/cmd/openapi-gen \
   --report-filename "${TRAINING_OPERATOR_ROOT}/hack/violation_exception_v2alpha1.list" \
   "${TRAINING_OPERATOR_ROOT}/pkg/apis/kubeflow.org/v2alpha1"
 
-# # Generating OpenAPI spec for Training Operator V1.
-# echo "Generating OpenAPI specification for kubeflow.org/v1"
-# kube::codegen::gen_openapi \
-#   --boilerplate "${TRAINING_OPERATOR_ROOT}/hack/boilerplate/boilerplate.go.txt" \
-#   --output-pkg "${TRAINING_OPERATOR_PKG}/pkg/apis/kubeflow.org/v1" \
-#   --output-dir "${TRAINING_OPERATOR_ROOT}/pkg/apis/kubeflow.org/v1" \
-#   --report-filename "${TRAINING_OPERATOR_ROOT}/hack/violation_exception_v1.list" \
-#   --update-report \
-#   "${TRAINING_OPERATOR_ROOT}/pkg/apis/kubeflow.org/v1"
-
-# # Generating OpenAPI spec for Training Operator V2.
-# echo "Generating OpenAPI specification for kubeflow.org/v2alpha1"
-# kube::codegen::gen_openapi \
-#   --boilerplate "${TRAINING_OPERATOR_ROOT}/hack/boilerplate/boilerplate.go.txt" \
-#   --output-pkg "${TRAINING_OPERATOR_PKG}/pkg/apis/kubeflow.org/v2alpha1" \
-#   --output-dir "${TRAINING_OPERATOR_ROOT}/pkg/apis/kubeflow.org/v2alpha1" \
-#   --report-filename "${TRAINING_OPERATOR_ROOT}/hack/violation_exception_v2alpha1.list" \
-#   --update-report \
-#   "${TRAINING_OPERATOR_ROOT}/pkg/apis/kubeflow.org/v2alpha1"
-
 # Generating OpenAPI Swagger for Training Operator V2.
 echo "Generate OpenAPI Swagger for kubeflow.org/v2alpha1"
 go run hack/swagger-v2/main.go >api.v2/openapi-spec/swagger.json
