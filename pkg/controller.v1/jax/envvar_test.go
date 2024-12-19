1ed9b7d2c9af367bff839a476ff2c18f42afea25
Sandipan Panda <samparksandipan@gmail.com>
2024-12-19 23:11:37 +0530

    Update JAXJob CI images
    Signed-off-by: Sandipan Panda <samparksandipan@gmail.com>

--- pkg/controller.v1/jax/envvar_test.go
+++ pkg/controller.v1/jax/envvar_test.go
@@ -30,7 +30,7 @@ func TestSetPodEnv(t *testing.T) {
 						Spec: corev1.PodSpec{
 							Containers: []corev1.Container{{
 								Name:  "jax",
-								Image: "docker.io/kubeflow/jaxjob-simple:latest",
+								Image: "docker.io/kubeflow/jaxjob-dist-spmd-mnist:latest",
 								Ports: []corev1.ContainerPort{{
 									Name:          kubeflowv1.JAXJobDefaultPortName,
 									ContainerPort: validPort,
