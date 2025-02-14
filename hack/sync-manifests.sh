c0b1d7d26e7a7caea1ffd8b3111b79be6de24b78
Yi Chen <github@chenyicn.net>
2025-02-14 18:48:57 +0800

    Add license to generated manifest files
    Signed-off-by: Yi Chen <github@chenyicn.net>

--- hack/sync-manifests.sh
+++ hack/sync-manifests.sh
@@ -22,7 +22,6 @@ echo "Syncing Kustomize manifests from manifests templated by trainer Helm chart
 set -o errexit
 set -o nounset
 set -o pipefail
-set -o xtrace
 
 # Helm chart release name and namespace.
 TRAINER_CHART_DIR=charts/trainer
@@ -41,7 +40,9 @@ DST_RUNTIMES_DIR=manifests/base/runtimes
 
 MANIFESTS_FILE=$(mktemp -t kubeflow-trainer-manifests.yaml)
 FIND_EXCLUDE_ARGS="-not -name kustomization.yaml -not -name kustomization.yml"
-YQ_ARGS='.metadata.labels."app.kubernetes.io/managed-by" = "Kustomize" | del(.metadata.labels."helm.sh/chart")'
+YEAR=$(date +%Y)
+LICENSE=$(cat hack/boilerplate/boilerplate.go.txt | sed 's|//|#|' | sed "s|YEAR|${YEAR}|g")
+YQ_ARGS=".metadata.labels.\"app.kubernetes.io/managed-by\" = \"Kustomize\" | del(.metadata.labels.\"helm.sh/chart\") | ... comments=\"\""
 
 setup() {
     # yq is required to parse yaml files.
@@ -63,24 +64,40 @@ update_crds() {
 }
 
 update_rbac() {
-    yq -e "select(.kind == \"ServiceAccount\" and .metadata.name == \"kubeflow-trainer-controller\") | ${YQ_ARGS}" "${MANIFESTS_FILE}" > ${DST_RBAC_DIR}/serviceaccount.yaml
-    yq -e "select(.kind == \"ClusterRole\" and .metadata.name == \"kubeflow-trainer-controller\") | ${YQ_ARGS}" "${MANIFESTS_FILE}" > ${DST_RBAC_DIR}/clusterrole.yaml
-    yq -e "select(.kind == \"ClusterRoleBinding\" and .metadata.name == \"kubeflow-trainer-controller\") | ${YQ_ARGS}" "${MANIFESTS_FILE}" > ${DST_RBAC_DIR}/clusterrolebinding.yaml
-    yq -e "select(.kind == \"Role\" and .metadata.name == \"kubeflow-trainer-controller\") | ${YQ_ARGS}" "${MANIFESTS_FILE}" > ${DST_RBAC_DIR}/role.yaml
-    yq -e "select(.kind == \"RoleBinding\" and .metadata.name == \"kubeflow-trainer-controller\") | ${YQ_ARGS}" "${MANIFESTS_FILE}" > ${DST_RBAC_DIR}/rolebinding.yaml
+    printf '#\n%s\n#\n\n' "${LICENSE}" > ${DST_RBAC_DIR}/serviceaccount.yaml
+    yq -e "select(.kind == \"ServiceAccount\" and .metadata.name == \"kubeflow-trainer-controller\") | ${YQ_ARGS}" "${MANIFESTS_FILE}" >> ${DST_RBAC_DIR}/serviceaccount.yaml
+
+    printf '#\n%s\n#\n\n' "${LICENSE}" > ${DST_RBAC_DIR}/clusterrole.yaml
+    yq -e "select(.kind == \"ClusterRole\" and .metadata.name == \"kubeflow-trainer-controller\") | ${YQ_ARGS}" "${MANIFESTS_FILE}" >> ${DST_RBAC_DIR}/clusterrole.yaml
+
+    printf '#\n%s\n#\n\n' "${LICENSE}" > ${DST_RBAC_DIR}/clusterrolebinding.yaml
+    yq -e "select(.kind == \"ClusterRoleBinding\" and .metadata.name == \"kubeflow-trainer-controller\") | ${YQ_ARGS}" "${MANIFESTS_FILE}" >> ${DST_RBAC_DIR}/clusterrolebinding.yaml
+
+    printf '#\n%s\n#\n\n' "${LICENSE}" > ${DST_RBAC_DIR}/role.yaml
+    yq -e "select(.kind == \"Role\" and .metadata.name == \"kubeflow-trainer-controller\") | ${YQ_ARGS}" "${MANIFESTS_FILE}" >> ${DST_RBAC_DIR}/role.yaml
+
+    printf '#\n%s\n#\n\n' "${LICENSE}" > ${DST_RBAC_DIR}/rolebinding.yaml
+    yq -e "select(.kind == \"RoleBinding\" and .metadata.name == \"kubeflow-trainer-controller\") | ${YQ_ARGS}" "${MANIFESTS_FILE}" >> ${DST_RBAC_DIR}/rolebinding.yaml
 }
 
 update_controller() {
-    yq -e "select(.kind == \"Deployment\" and .metadata.name == \"kubeflow-trainer-controller\") | ${YQ_ARGS}" "${MANIFESTS_FILE}" > ${DST_CONTROLLER_DIR}/deployment.yaml
-    yq -e "select(.kind == \"Service\" and .metadata.name == \"kubeflow-trainer-controller-service\" | ${YQ_ARGS})" "${MANIFESTS_FILE}" > ${DST_CONTROLLER_DIR}/service.yaml
+    printf '#\n%s\n#\n\n' "${LICENSE}" > ${DST_CONTROLLER_DIR}/deployment.yaml
+    yq -e "select(.kind == \"Deployment\" and .metadata.name == \"kubeflow-trainer-controller\") | ${YQ_ARGS}" "${MANIFESTS_FILE}" >> ${DST_CONTROLLER_DIR}/deployment.yaml
+
+    printf '#\n%s\n#\n\n' "${LICENSE}" > ${DST_CONTROLLER_DIR}/service.yaml
+    yq -e "select(.kind == \"Service\" and .metadata.name == \"kubeflow-trainer-controller-service\") | ${YQ_ARGS}" "${MANIFESTS_FILE}" >> ${DST_CONTROLLER_DIR}/service.yaml
 }
 
 update_webhook() {
-    yq -e "select(.kind == \"Secret\" and .metadata.name == \"kubeflow-trainer-webhook-cert\") | ${YQ_ARGS}" "${MANIFESTS_FILE}" > ${DST_WEBHOOK_DIR}/secret.yaml
-    yq -e "select(.kind == \"ValidatingWebhookConfiguration\" and .metadata.name == \"kubeflow-trainer-validating-webhook\" | ${YQ_ARGS})" "${MANIFESTS_FILE}" > ${DST_WEBHOOK_DIR}/validatingwebhookconfiguration.yaml
+    printf '#\n%s\n#\n\n' "${LICENSE}" > ${DST_WEBHOOK_DIR}/secret.yaml
+    yq -e "select(.kind == \"Secret\" and .metadata.name == \"kubeflow-trainer-webhook-cert\") | ${YQ_ARGS}" "${MANIFESTS_FILE}" >> ${DST_WEBHOOK_DIR}/secret.yaml
+
+    printf '#\n%s\n#\n\n' "${LICENSE}" > ${DST_WEBHOOK_DIR}/validatingwebhookconfiguration.yaml
+    yq -e "select(.kind == \"ValidatingWebhookConfiguration\" and .metadata.name == \"kubeflow-trainer-validating-webhook\" | ${YQ_ARGS})" "${MANIFESTS_FILE}" >> ${DST_WEBHOOK_DIR}/validatingwebhookconfiguration.yaml
 }
 
 update_runtimes() {
+    printf '#\n%s\n#\n\n' "${LICENSE}" > ${DST_RUNTIMES_DIR}/pretraining/torch-distributed.yaml
     yq -e "select(.kind == \"ClusterTrainingRuntime\" and .metadata.name == \"torch-distributed\")" "${MANIFESTS_FILE}" > ${DST_RUNTIMES_DIR}/pretraining/torch-distributed.yaml
 }
 
